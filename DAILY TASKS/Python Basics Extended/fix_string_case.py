664ee38f9a90789073a7ba8f6d5be2c97a46f140
Alexandr <olexandrzhezheria@gmail.com>
2023-04-04 05:09:51 +0300

    add DAILY TASKS

--- DAILY TASKS/Python Basics Extended/fix_string_case.py
+++ DAILY TASKS/Python Basics Extended/fix_string_case.py
@@ -1,25 +1,6 @@
 # У цьому завданні тобі буде дано рядок, в якому можуть бути присутніми як великі, так і малі літери. Тобі потрібно написати функцію fix_string_case, яка перетворює рядок або тільки на нижній, або на верхній регістр залежно від того, яка кількість літер конкретного регістру переважає у рядку.
 # Якщо рядок містить однакову кількість великих і малих літер, перетвори рядок на нижній регістр.
 
-# def fix_string_case(word: str) -> str:
-#     uppercase_count = 0
-#     lowercase_count = 0
-#
-#     # Рахуємо кількість великих і малих літер у рядку
-#     for c in word:
-#         if c.isupper():
-#             uppercase_count += 1
-#         else:
-#             lowercase_count += 1
-#
-#     # Перевіряємо, якого регістру в рядку більше
-#     if uppercase_count > lowercase_count:
-#         return word.upper()
-#     elif uppercase_count >= lowercase_count:
-#         return word.lower()
-#     else:
-#         return word.lower()
-
 def fix_string_case(word: str) -> str:
     lower_count = 0
     upper_count = 0
