52b8ba52085cfa2797cc1e8a584cba3ddb54b48a
Alexandr <olexandrzhezheria@gmail.com>
2023-04-13 00:20:25 +0300

    add Pytest_Lesson

--- Decorators/error_handler.py
+++ Decorators/error_handler.py
@@ -6,7 +6,25 @@
 # *callback_args та **callback_kwargs, аргументи, які потрібно передати в callback
 # Такий декоратор може бути корисним у великій кількості випадків. Наприклад, ми використовуємо функцію, яка має надсилати дуже важливу інформацію одному з наших сервісів. У випадку, якщо вона не пройшла (через інтернет, погоду або поганий гороскоп), ми хочемо, щоб сповіщення про це надсилалося на наш email. Це завдання можна легко вирішити за допомогою такого декоратора:
 
-# write code below this line
+from typing import Any
+
+
+def error_handler(
+        error_type: str,
+        callback: Any,
+        *callback_args: Any,
+        **callback_kwargs: Any
+) -> Any:
+    def wrapper(func: Any) -> Any:
+        def inner(*args, **kwargs) -> Any:
+            try:
+                return func(*args, **kwargs)
+            except error_type:
+                return callback(*callback_args, **callback_kwargs)
+
+        return inner
+
+    return wrapper
 
 
 def func_produce_value_error() -> int:
@@ -14,17 +32,29 @@ def func_produce_value_error() -> int:
     return int("0.2")
 
 
+@error_handler(ValueError, lambda: "Bye, errors")
+@error_handler(TypeError, func_produce_value_error)
 def func_produce_type_error() -> str:
     print("Hello, TypeError")
     return 10 + "10"
 
-def send_alert_to_the_email(email: str):
-    send_email(email, "We have some troubles!")
 
-@error_handler(SendingInfoError, send_alert_to_the_email, "admin123@company")
-def send_important_information(information):
-    print("Sending important information...")
-    server.send(information) # May produce an error
+# def func_produce_value_error() -> int:
+#     print("Hello, ValueError")
+#     return int("0.2")
+#
+#
+# def func_produce_type_error() -> str:
+#     print("Hello, TypeError")
+#     return 10 + "10"
+#
+# def send_alert_to_the_email(email: str):
+#     send_email(email, "We have some troubles!")
+#
+# @error_handler(SendingInfoError, send_alert_to_the_email, "admin123@company")
+# def send_important_information(information):
+#     print("Sending important information...")
+#     server.send(information) # May produce an error
 
 # Застосуй декоратор для функції func_produce_type_error, яку вже оголошено у файлі. Передай як callback функцію func_produce_value_error. Але оскільки вона теж викликає помилку, то застосуй до функції func_produce_type_error ще один callback, який вже оброблятиме помилку ValueError. В якості обробника передай функцію, яка повертає рядок Bye, errors.
 #
