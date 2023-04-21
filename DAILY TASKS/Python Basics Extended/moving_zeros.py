aa84b7da314f61bc348913929ac379fac9f3d015
Alexandr <olexandrzhezheria@gmail.com>
2023-04-21 11:00:57 +0300

    Daily Tasks

--- DAILY TASKS/Python Basics Extended/moving_zeros.py
+++ DAILY TASKS/Python Basics Extended/moving_zeros.py
@@ -14,7 +14,7 @@ def moving_zeros(nums: list) -> list:
 
 # Приклади:
 
-move_zeros([1, 0, 1, 2, 0, 1, 3]) # повертає [1, 1, 2, 1, 3, 0, 0]
-move_zeros([0, 2, 0, 1, 1, 0, 3]) # повертає [2, 1, 1, 3, 0, 0, 0]
-move_zeros([0,0]) # повертає [0, 0]
-move_zeros([]) # повертає []
+print(moving_zeros([1, 0, 1, 2, 0, 1, 3])) # повертає [1, 1, 2, 1, 3, 0, 0]
+print(moving_zeros([0, 2, 0, 1, 1, 0, 3])) # повертає [2, 1, 1, 3, 0, 0, 0]
+print(moving_zeros([0,0])) # повертає [0, 0]
+print(moving_zeros([])) # повертає []
