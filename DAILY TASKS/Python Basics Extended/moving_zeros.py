d89c23b4f2d6810c8c44a39a39299d29b5f3a643
Alexandr <olexandrzhezheria@gmail.com>
2023-04-21 10:55:10 +0300

    add Pytest_Lesson

---
+++ DAILY TASKS/Python Basics Extended/moving_zeros.py
@@ -0,0 +1,20 @@
+# Напиши функцію moving_zeros яка приймає список nums, та повертає список, в котрому всі нулі переміщені в кінець.
+# Збережи порядок решти елементів.
+
+def moving_zeros(nums: list) -> list:
+    zeros = []
+    non_zeros = []
+    for num in nums:
+        if num == 0:
+            zeros.append(num)
+        else:
+            non_zeros.append(num)
+    return non_zeros + zeros
+
+
+# Приклади:
+
+move_zeros([1, 0, 1, 2, 0, 1, 3]) # повертає [1, 1, 2, 1, 3, 0, 0]
+move_zeros([0, 2, 0, 1, 1, 0, 3]) # повертає [2, 1, 1, 3, 0, 0, 0]
+move_zeros([0,0]) # повертає [0, 0]
+move_zeros([]) # повертає []
